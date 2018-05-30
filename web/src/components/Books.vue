3d734b67a3e1ab7891f870412d9e29dc68353da5
Devin Dodson <git@devindodson.com>
2018-05-29 18:33:33 -0700

    Added book view, incorporated update functionality

--- web/src/components/Books.vue
+++ web/src/components/Books.vue
@@ -1,70 +1,103 @@
 <template>
-	<div>
-		<div v-if="book">
-			<h2>Book Detail</h2>
-			<p>
-				<strong>Title:</strong> {{book.title}}</p>
-			<p>
-				<strong>Author:</strong> {{book.author}}</p>
-			<p>
-				<strong>Summary:</strong> {{book.summary}}</p>
-			<p>
-				<strong>Published:</strong> {{book.published}}</p>
-			<p>
-				<!-- <strong>Checked out by: -->
-					<!-- <ul> -->
-						<!-- <li v-for="book of user.books" :key="book.id">
-							<p>
-								<strong>Title:</strong> {{book.title}}</p>
-							<p>
-								<strong>Author:</strong> {{book.author}}</p>
-							<router-link :to="{name: 'Book', params: { id: book._id}}">
-								More info
-							</router-link>
-						</li> -->
-					<!-- </ul> -->
-				<!-- </strong> -->
-			</p>
-		</div>
-		<div v-else>
-			Sorry, we couldn't find that book's information
-		</div>
+	<div class="modal-card">
+		<header class="modal-card-head">
+			<p class="modal-card-title">Book Detail</p>
+			<a>See all books</a>
+		</header>
+		<section class="modal-card-body">
+			<b-field label="Title">
+				<b-input type="text" v-model="book.title">
+				</b-input>
+			</b-field>
+			<b-field label="Author">
+				<b-input type="text" v-model="book.author">
+				</b-input>
+			</b-field>
+			<b-field label="Summary">
+				<b-input type="textarea" v-model="book.summary">
+				</b-input>
+			</b-field>
+			<b-field label="Published">
+				<b-input type="text" v-model="book.published">
+				</b-input>
+			</b-field>
+		</section>
+		<footer class="modal-card-foot spaced-children">
+			<button class="button is-success" @click="saveChanges">Save Changes</button>
+			<button class="button is-danger" @click="deleteBook">Delete</button>
+		</footer>
 	</div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue from "vue";
 export default {
-  name: "Book",
-  props: ["bookId"],
-  data() {
-    return {
-      book: null,
-      checkedOutByUsers: []
-    };
-  },
-  created() {
-    axios
-      .get(`http://localhost:3000/book/${this.$route.params.id}`)
-      .then(response => {
-        console.log(response.data);
-        this.book = response.data;
-      })
-      .catch(e => {
-        console.error(e);
-      });
-    axios
-      .get(`http://localhost:3000/book/${this.$route.params.id}`)
-      .then(response => {
-        console.log(response.data);
-        this.book = response.data;
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  }
+	name: "Book",
+	data() {
+		return {
+			book: null,
+			checkedOutByUsers: []
+			// readablePublishedDate: ""
+		};
+	},
+	methods: {
+		saveChanges() {
+			// console.log(this.book);
+			axios
+				.put(
+					`http://localhost:3000/book/${this.$route.params.id}`,
+					this.book
+				)
+				.then(response => {
+					console.log(response);
+					this.book = response.data;
+					Vue.set(
+						this.book,
+						"published",
+						new Date(response.data.published).toLocaleDateString()
+					);
+					this.book.published = new Date(
+						response.data.published
+					).toLocaleDateString();
+				})
+				.catch(e => {
+					console.error(e);
+				});
+		},
+		deleteBook() {}
+	},
+	computed: {},
+	created() {
+		axios
+			.get(`http://localhost:3000/book/${this.$route.params.id}`)
+			.then(response => {
+				this.book = response.data;
+			})
+			.catch(e => {
+				console.error(e);
+			});
+		axios
+			.get(`http://localhost:3000/book/${this.$route.params.id}`)
+			.then(response => {
+				this.book = response.data;
+				Vue.set(
+					this.book,
+					"published",
+					new Date(response.data.published).toLocaleDateString()
+				);
+
+				// this.book.published = ;
+			})
+			.catch(e => {
+				console.error(e);
+			});
+	}
 };
 </script>
 
 <style>
+.spaced-children {
+	justify-content: space-between;
+}
 </style>
