c798c4f0e86ed8b7e03ccbde018ee8c05761dbcf
Devin Dodson <git@devindodson.com>
2018-05-29 18:34:39 -0700

    Incorporated modal book view

--- web/src/components/Dashboard.vue
+++ web/src/components/Dashboard.vue
@@ -13,8 +13,7 @@
 			<div class="container">
 				<div class="level">
 					<div class="level-right">
-						<div class="level-item">
-							Showing
+						<div class="level-item inline"> Showing
 							<b-input type="number" v-model="perPage" size="is-small" min="1"></b-input>
 							users per page
 						</div>
@@ -30,7 +29,7 @@
 						</b-table-column>
 
 						<b-table-column field="email" label="Email" sortable>
-							{{ props.row.email }}
+							<a :href="`mailto:${props.row.email}`">{{props.row.email}}</a>
 						</b-table-column>
 
 						<b-table-column field="dateOfBirth" label="Date of Birth" sortable centered>
@@ -44,7 +43,7 @@
 										<strong>Title:</strong> {{book.title}}</p>
 									<p>
 										<strong>Author:</strong> {{book.author}}</p>
-									<router-link :to="{name: 'Book', params: { id: book._id}}">
+									<router-link @click.native="isModalActive=true" :to="{name: 'Book', params: { id: book._id}}">
 										More info
 									</router-link>
 								</li>
@@ -54,41 +53,17 @@
 				</b-table>
 			</div>
 		</section>
-
-		<!-- <ul v-if="users && users.length">
-			<li v-for="user of users" :key="user.id">
-				<h3>
-					<strong>{{user.name}}</strong>
-				</h3>
-				<p>
-					<strong>Email:</strong> {{user.email}}</p>
-				<p>
-					<strong>Date of birth:</strong> {{user.dateOfBirth}}</p>
-				<p v-if="user.books && user.books.length">
-					<strong>Books checked out:</strong>
-					<ul>
-						<li v-for="book of user.books" :key="book.id">
-							<p>
-								<strong>Title:</strong> {{book.title}}</p>
-							<p>
-								<strong>Author:</strong> {{book.author}}</p>
-							<router-link :to="{name: 'Book', params: { id: book._id}}">
-								More info
-							</router-link>
-						</li>
-					</ul>
-				</p>
-				<p v-else>
-					<strong class="no-books">No books checked out by this user</strong>
-				</p>
-			</li>
-		</ul> -->
+		<b-modal :active.sync="isModalActive" has-modal-card>
+			<router-view>
+			</router-view>
+		</b-modal>
 	</div>
 </template>
 
 <script>
 import Vue from "vue";
 import axios from "axios";
+import Books from "./Books";
 
 export default {
 	name: "Dashboard",
@@ -97,33 +72,10 @@ export default {
 			currentPage: 1,
 			perPage: 20,
 			users: [],
-			columns: [
-				{
-					field: "name",
-					label: "Name",
-					sortable: true
-				},
-				{
-					field: "email",
-					label: "Email",
-					sortable: true
-				},
-				{
-					field: "dateOfBirth",
-					label: "Date of Birth",
-					sortable: true
-				},
-				{
-					field: "booksCheckedOut",
-					label: "Books checked out"
-					// sortable: true
-				}
-			]
+			isModalActive: false
 		};
 	},
 	created() {
-		// const defaultLimit = 10;
-		// const defaultSort = 10;
 		console.log(this.$route.params.content);
 		axios
 			.get(`http://localhost:3000/user?${this.$route.params.content}`)
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,34 +1,42 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Dashboard from '@/components/Dashboard';
-import Books from '@/components/Books';
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import Books from "@/components/Books";
 import Buefy from "buefy";
 import "buefy/lib/buefy.css";
 import "@mdi/font/css/materialdesignicons.min.css";
 
 Vue.use(Buefy);
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
       path: "/:content?",
       name: "Dashboard",
       component: Dashboard,
-      meta: {title: 'Home'},
-    },
+      meta: { title: "Home" },
+      children: [
+        {
+          path: "/book/:id",
+          name: "Book",
+          component: Books,
+          props: true
+        }
+      ]
+    }
     // {
     //   path: "/user/:id",
     //   name: "User",
     //   component: Users
     // },
-    {
-      path: "/book/:id",
-      name: "Book",
-      component: Books,
-      props: true
-    }
+    // {
+    //   path: "/book/:id",
+    //   name: "Book",
+    //   component: Books,
+    //   props: true
+    // }
   ]
 });
