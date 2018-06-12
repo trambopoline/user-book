bcf38a19ddd952ec8e0f83f57e470622b03f0497
Devin Dodson <git@devindodson.com>
2018-06-11 17:18:07 -0700

    Passing environment variables from Docker to Webpack

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -1,4 +1,6 @@
-'use strict'
+"use strict";
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  USER_URL: JSON.stringify(process.env.USER_URL),
+  BOOK_URL: JSON.stringify(process.env.BOOK_URL)
+};
